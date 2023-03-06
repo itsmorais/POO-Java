d1e080b556b96f4e66aed9472cbc5a29e5b7d3c1
Michael morais <53665466+itsmorais@users.noreply.github.com>
2023-03-06 10:06:52 -0300

    PraticaJava06032023

---
+++ Pratica1/example.java
@@ -0,0 +1,12 @@
+package aula;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 0; i <= 5 ;i++){
+            System.out.println(i);}
+    }
+    
+}
