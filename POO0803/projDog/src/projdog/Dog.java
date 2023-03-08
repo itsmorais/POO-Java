c2a31b7ad87b2f864f0d2b3811205aee025ce1b8
Michael morais <53665466+itsmorais@users.noreply.github.com>
2023-03-08 08:31:39 -0300

    Update Dog.java

--- POO0803/projDog/src/projdog/Dog.java
+++ POO0803/projDog/src/projdog/Dog.java
@@ -2,14 +2,30 @@
 package projdog;
 
 public class Dog {
-    String nome;
-    int peso;
+    private String nome;
+    private int peso;
+            
+    public String getNome(){
+        return nome;
+    }    
+
+    public int getPeso(){
+        return peso;
+    }
+    
+    public void setNome(String n){
+        nome=n;
+    }
+    public void setPeso(int p){
+        peso=p;
+    }
+    
     
     void latir(){
         if (peso >= 9){
-            System.out.println(nome+": AUUUUUUUUUUU!");
+            System.out.println(getNome()+": AUUUUUUUUUUU!");
         }else if(peso < 9){
-            System.out.println(nome+": au");
+            System.out.println(getNome()+": au");
         }
     }
     
