c0cad87c109cf4a02e8d6ae238b5e6e91f4b0585
Michael Morais <michael@nexusbr.com>
2023-04-24 10:20:11 -0300

    Aula listas em Java

--- vetores/src/App.java
+++ vetores/src/App.java
@@ -1,35 +1,13 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-       Scanner ler = new Scanner(System.in);
-        int vetA[];
-        int vetB[];
-       
-        vetA = new int[10];
-        vetB = new int[10];
-        int soma = 0;
+        int x = 0;
+        int y = 0;
+        int z = 0;
 
-        // Entrada
-        for(int i = 0; i < 10; i++){
-            System.out.printf("Insira valores para o vetor A. %dº valor: ",i+1);
-            vetA[i] = ler.nextInt();    
-            
+        for(z=0; z<5; z++){
+            x += (x+y < 3) ? ++x : x++;
+            y += (y > 3 || z == 1) ? ++y : y++;
+            System.out.printf("Z: %d X: %d Y: %d \n",z,x,y);
         }
-        for(int i = 0; i < 10; i++){   
-            System.out.printf("%d° valor para o Vetor B: ",i+1);
-            vetB[i] = ler.nextInt();
-        }
-
-        // Calculo do produto escalar + saída
-        System.out.print("O produto escalar de A por B é: ");
-        for(int i = 0; i<10;i++){
-            soma += vetA[i] * vetB[i];
-        }
-
-        System.out.println(soma);
-      
-
     }    
-        }
-
+}
